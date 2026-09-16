b27090f1e0af9dbc2ef1fc81f18376ac28635034
John Doe <johndoe@example.com>
2026-09-16 11:46:08 +0800

    初始化仓库：配置环境并使用相对路径

---
+++ problemset/231A-Team.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> // 万能头文件，包含了所有标准库（CF 完美支持）
+using namespace std;    // 省略 std:: 前缀
+
+void solve() {
+    // 这里写单组测试数据的逻辑
+    int n;
+    if (!(cin >> n)) return;
+    int count=0,a,b,c;
+    for (int i = 0; i < n; i++) {
+        cin>>a>>b>>c;
+        if(a+b+c>=2){
+            count++;
+        }
+    }
+    // 业务逻辑...
+    cout << count;
+}
+
+int main() {
+    // 核心：解除 cin/cout 与 C 标准流的同步，加速 I/O，防止 TLE
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // 如果题目有多组测试用例（如 CF 常见的 t 组输入），取消下行注释：
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ 模版.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> // 万能头文件，包含了所有标准库（CF 完美支持）
+using namespace std;    // 省略 std:: 前缀
+
+void solve() {
+    // 这里写单组测试数据的逻辑
+    int n;
+    if (!(cin >> n)) return;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    // 业务逻辑...
+    cout << "ans\n";
+}
+
+int main() {
+    // 核心：解除 cin/cout 与 C 标准流的同步，加速 I/O，防止 TLE
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // 如果题目有多组测试用例（如 CF 常见的 t 组输入），取消下行注释：
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
